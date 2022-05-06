12ea21651beb5665ca0b1d6b9b145608067e17b3
YACHNIKMIKHAIL <limerick2011@yandex.by>
2022-05-06 17:55:44 +0300

    this.socket?.disconnect() commit

--- src/ChatAPI.ts
+++ src/ChatAPI.ts
@@ -1,8 +1,9 @@
-import {io} from "socket.io-client";
+import {io, Socket} from "socket.io-client";
 import {MessageType} from "./App";
+import {destroyConnection} from "./ChatReducer";
 
 // const socket = io('http://localhost:8000/')
-const socket = io('https://my-chat-back.herokuapp.com/')
+// const socket = io('https://my-chat-back.herokuapp.com/')
 
 socket.on('init-messages-published', (messagesOnBack: MessageType[]) => {
     setMessages(messagesOnBack)
@@ -10,3 +11,15 @@ socket.on('init-messages-published', (messagesOnBack: MessageType[]) => {
 socket.on('new-message-sent', (newMessage: MessageType) => {
     setMessages((messages) => [...messages, newMessage])
 })
+
+const chatAPI = {
+    socket: null as null | Socket,
+    createConnection() {
+        // this.socket = io('http://localhost:8000/')
+        this.socket = io('https://my-chat-back.herokuapp.com/')
+    },
+    destroyConnection() {
+        this.socket?.disconnect()
+        this.socket = null
+    }
+}
